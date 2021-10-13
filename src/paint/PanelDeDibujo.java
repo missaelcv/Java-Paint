bfb3adca20ad3bfbda9e9164b50db13b56d5893c
missaelcv <missaelcv0220@gmail.com>
2021-10-12 23:03:02 -0400

    Creando Clase PanelDeDibujo

---
+++ src/paint/PanelDeDibujo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paint;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Misael Caceres
+ */
+public class PanelDeDibujo extends JPanel {
+}
