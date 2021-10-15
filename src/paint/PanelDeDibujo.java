cd494f2987c581978a40944b63c14f0130d4622b
missaelcv <missaelcv0220@gmail.com>
2021-10-14 23:26:56 -0400

    this.addMouseListener(new MouseListener()

--- src/paint/PanelDeDibujo.java
+++ src/paint/PanelDeDibujo.java
@@ -7,6 +7,9 @@ package paint;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JPanel;
 
 /**
@@ -15,16 +18,39 @@ import javax.swing.JPanel;
  */
 public class PanelDeDibujo extends JPanel {
     
+    Point puntoInicial;
+    Point puntoFinal;
     public PanelDeDibujo () {
-        setBackground  (Color.WHITE);     
-    }
+        setBackground  (Color.WHITE);    
+        
+        this.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent me) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
 
-    /**
-     *
-     * @param g
-     */
-    
-    //@override
+            @Override
+            public void mousePressed(MouseEvent me) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent me) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent me) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void mouseExited(MouseEvent me) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+ }
+    //@Override
     protected void paintComponet(Graphics g) {
         super.paintComponent(g);
         
