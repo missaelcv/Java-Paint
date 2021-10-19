070a9cdcdfce401ab23230eb70daa2ae23c77b10
missaelcv <missaelcv0220@gmail.com>
2021-10-18 21:14:31 -0400

    crear paquete figura

---
+++ src/figuras/DibujoLibre.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package figuras;
+
+import java.awt.Graphics;
+import java.awt.Point;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Misael Caceres
+ */
+public class DibujoLibre extends Figura {
+
+    ArrayList<Point> puntos;
+
+    public DibujoLibre(Point puntoInicial) {
+        puntos = new ArrayList<>();
+        actualizar(puntoInicial);
+    }
+
+    @Override
+    public void actualizar(Point puntoFinal) {
+        puntos.add(puntoFinal);
+    }
+
+    @Override
+    public void dibujar(Graphics g) {
+        if (puntos.size()==1) {
+            g.drawLine(puntos.get(0).x, puntos.get(0).y, puntos.get(0).x, puntos.get(0).y);
+        }
+        
+        for (int i = 1; i < puntos.size(); i++) {
+            g.drawLine(puntos.get(i - 1).x, puntos.get(i - 1).y, puntos.get(i).x, puntos.get(i).y);
+        }
+    }
+}
+
+//    BufferedImage papel;
+//    Graphics2D crallon;
+
---
+++ src/figuras/Linea.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package figuras;
+
+/**
+ *
+ * @author Misael Caceres
+ */
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
+/**
+ *
+ * @author josearielpereyra
+ */
+public class Linea extends Figura{
+    Point puntoInicial;
+    Point puntoFinal;
+
+    public Linea(Point puntoInicial, Point puntoFinal) {
+        this.puntoInicial = puntoInicial;
+        this.puntoFinal = puntoFinal;
+    }
+
+    public Linea(Point puntoInicial) {
+        this(puntoInicial, puntoInicial);
+    }
+
+    public void actualizar( Point puntoFinal ) {
+        this.puntoFinal = puntoFinal;
+    }
+
+
+    public void dibujar(Graphics g) {
+        if(puntoInicial != null && puntoFinal != null) {
+            g.setColor(Color.black);
+            //g.drawLine( puntoInicial.x, puntoInicial.y, puntoFinal.x, puntoFinal.y );
+           g.drawLine( puntoInicial.x, puntoInicial.y, puntoFinal.x, puntoFinal.y );
+           g.drawRect(puntoInicial.x, puntoInicial.y, puntoFinal.x-puntoInicial.x, puntoFinal.y-puntoInicial.y);
+           g.drawOval(puntoInicial.x, puntoInicial.y, puntoFinal.x-puntoInicial.x, puntoFinal.y-puntoInicial.y);
+        }
+    }
+}
