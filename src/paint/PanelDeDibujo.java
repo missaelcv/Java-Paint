91cc9d88eee01a8b9b35f1fd0f0a3cef1b1af7a5
missaelcv <missaelcv0220@gmail.com>
2021-10-18 21:17:04 -0400

    metodos privados

--- src/paint/PanelDeDibujo.java
+++ src/paint/PanelDeDibujo.java
@@ -5,12 +5,18 @@
  */
 package paint;
 
+import figuras.DibujoLibre;
+import figuras.Figura;
+import figuras.Linea;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 /**
  *
@@ -18,78 +24,88 @@ import javax.swing.JPanel;
  */
 public class PanelDeDibujo extends JPanel {
     
-    Point puntoInicial;
-    Point puntoFinal;
-    public PanelDeDibujo () {
-        setBackground  (Color.WHITE);    
-        
-        class Manejador implements MouseListener {
 
+/**
+ *
+ * @author josearielpereyra
+ */
+public class PanelDeDibujo extends JPanel {
+
+    Figura figuraActual;
+    ArrayList<Figura> figuras;
+   
+    DibujoLibre punto;
+
+    public PanelDeDibujo() {
+
+        figuras = new ArrayList<>();
+       
+
+        setBackground(Color.WHITE);
+
+        class Manejador extends MouseAdapter {
+
+            private Point punto = new Point();
+
+            //
             @Override
-            public void mouseClicked(MouseEvent me) {
+            public void mousePressed(MouseEvent evento) {
+                if (evento.getButton() == MouseEvent.BUTTON1) {
+                    System.out.println("Se presiono el boton 1");
+
+                    figuraActual = new Linea(evento.getPoint());
+                    figuras.add(figuraActual);
+                    repaint();
+                } 
+                
+                if (evento.getButton() == MouseEvent.BUTTON3) {
+                    System.out.println("Se presiono el boton 3");
+                    figuraActual = new DibujoLibre(evento.getPoint());
+                    figuras.add(figuraActual);
+                } 
             }
 
             @Override
-            public void mousePressed(MouseEvent me) {
-            }
+            public void mouseDragged(MouseEvent evento) {
 
-            @Override
-            public void mouseReleased(MouseEvent me) {
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent me) {
-            }
-
-            @Override
-            public void mouseExited(MouseEvent me) {
+                if (SwingUtilities.isLeftMouseButton(evento)) {
+                    System.out.println("Se arrastro el boton 1");
+                    figuraActual.actualizar(evento.getPoint());
+                }  
+                
+                if (SwingUtilities.isRightMouseButton(evento)) {
+                    System.out.println("Se arrastro el boton 3");
+                    figuraActual.actualizar(evento.getPoint());
+                }
+                repaint();
             }
         }
-        
-        
-        this.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent me) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
 
-            @Override
-            public void mousePressed(MouseEvent me) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent me) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent me) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-
-            @Override
-            public void mouseExited(MouseEvent me) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-        });
- }
-    //@Override
-    protected void paintComponet(Graphics g) {
-        super.paintComponent(g);
-        
-        g.setColor(Color.red);
-        
-        int distancia = 20;
-        int total =0;
-        for (int y = distancia; y < getHeight() && total < 27; y += distancia) {
-            total ++;
-        g.drawString(" "+  total, 0, y);
-        g.drawLine(10, y, getWidth() -10 ,y);}
-        
-        
-        g.setColor(Color.red);
-        g.drawLine(distancia * 2,0, distancia * 2, getHeight());
+        Manejador manejador = new Manejador();
+        this.addMouseListener(manejador);
+        this.addMouseMotionListener(manejador);
     }
-    
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // g.drawImage(papel, 0, 0, null);
+        g.setColor(Color.blue);
+
+        int distancia = 20;
+        int total = 0;
+        for (int y = distancia * 2; y < getHeight() && total < 27; y += distancia) {
+            total++;
+            g.drawString("" + total, 0, y);
+            g.drawLine(10, y, getWidth() - 10, y);
+        }
+
+        g.setColor(Color.red);
+        g.drawLine(distancia * 2, 0, distancia * 2, getHeight());
+
+        for (Figura figuraActual : figuras) {
+            figuraActual.dibujar(g);
+        }
+    }
 }
