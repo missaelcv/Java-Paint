295c6ea1460b7fd2be5dd4ba95c2f5bdca7d59df
missaelcv <missaelcv0220@gmail.com>
2021-10-19 22:56:23 -0400

    provando clase
    Clase Rectangulo

--- src/figuras/Rectangulo.java
+++ src/figuras/Rectangulo.java
@@ -25,7 +25,65 @@ public class Rectangulo extends Figura{
     }
     
     //Metodos Distitnos 
-         
-
     
+    private double longitud;
+    private double anchura;
+ 
+    public Rectangulo(double longitud, double anchura) {
+        this.longitud = longitud;
+        this.anchura = anchura;
+    }
+ 
+    public Rectangulo() {
+        this.longitud = 1;
+        this.anchura = 1;
+    }
+//Metodos para calcular el perimetro y el Aréa
+ 
+    public double getlongitud() {
+        return longitud;
+    }
+ 
+    public double getanchura() {
+        return anchura;
+    }
+ 
+    public void setlongitud(double longitud) {
+        if (longitud > 0.0 && longitud < 20.0) {
+            this.longitud = longitud;
+        }
+    }
+ 
+    public void setanchura(double anchura) {
+        if (anchura > 0.0 && longitud < 20.0) {
+            this.anchura = anchura;
+        } else {
+            throw new RuntimeException("la Anchura debe debe de ser > 0 < 20.0");
+        }
+    }
+ 
+    @Override
+    public String toString() {
+        return "Rectangulo{" + "longitud=" + longitud + ", anchura=" + anchura + '}';
+ 
+    }
+ 
+    ///////////////////////////
+    public double calcularArea() {
+        return anchura * longitud;
+    }
+ 
+    public double calculaPerimetro() {
+        return (2 * anchura) + (2 * longitud);
+    }
+    ///////////////////////////
+ 
+    public static void main(String[] args) {
+        Rectangulo rect = new Rectangulo();
+        rect.setanchura(-12.2);
+        rect.setlongitud(12.2);
+        System.out.println(rect.calcularArea());
+        System.out.println(rect.calculaPerimetro());
+    }
+         
 }
