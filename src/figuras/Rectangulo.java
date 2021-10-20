915d5722d756ce14373b64c629c526e410080553
missaelcv <missaelcv0220@gmail.com>
2021-10-19 22:42:54 -0400

    Rectangulo
    nuevo Rectangulo

---
+++ src/figuras/Rectangulo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package figuras;
+
+import java.awt.Graphics;
+import java.awt.Point;
+
+/**
+ *
+ * @author Misael Caceres
+ */
+public class Rectangulo extends Figura{
+
+    @Override
+    public void actualizar(Point puntoFinal) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void dibujar(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    //Metodos Distitnos 
+         
+
+    
+}
