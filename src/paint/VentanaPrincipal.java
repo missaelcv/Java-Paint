9f013a6cbdcba885a83986b880f5762b8abe020a
missaelcv <missaelcv0220@gmail.com>
2021-10-28 22:58:09 -0400

    cambio de main stactis
    cambio de public main static

--- src/paint/VentanaPrincipal.java
+++ src/paint/VentanaPrincipal.java
@@ -319,7 +319,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
+       java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new VentanaPrincipal().setVisible(true);
             }
