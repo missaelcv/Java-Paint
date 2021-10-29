31c9a704114164a868e3e0f6a72f71dac6198353
missaelcv <missaelcv0220@gmail.com>
2021-10-28 23:10:55 -0400

    cambio de Recntangulo

--- src/figuras/Rectangulo.java
+++ src/figuras/Rectangulo.java
@@ -2,43 +2,31 @@ package figuras;
 
 import java.awt.*;
 
-public class Rectangulo extends Figura {
-
+public class Rectangulo extends Figura{
     int x;
     int y;
     int anchura;
     int altura;
 
-    public Rectangulo(int x, int y, int anchura, int altura) {
-        this.x = x;
-        this.y = y;
-        this.anchura = anchura;
-        this.altura = altura;
+    public Rectangulo(Color color, Point puntoInicial) {
+        super(color);
+        this.x = puntoInicial.x;
+        this.y = puntoInicial.y;
+        this.anchura = 1;
+        this.altura = 1;
     }
 
-    public Rectangulo(Point puntoInicial) {
-        this(puntoInicial.x, puntoInicial.y,1,1);
-    }
-
-    public void actualizar(Point puntoActual) {
+    public void actualizar( Point puntoActual ) {
         this.anchura = puntoActual.x - x;
         this.altura = puntoActual.y - y;
     }
-    
-    //Esto es facil (si se sabe). 
-    //Agramonte.
 
     public void dibujar(Graphics g) {
+        g.setColor( getColorDePrimerPlano() );
         
-        int x = this.anchura < 0 ? this.x + this.anchura : this.x;
-        int y = this.altura < 0 ? this.y + this.altura : this.y;
-        int anchura = Math.abs(this.anchura);
-        int altura = Math.abs(this.altura);
-
-        g.setColor(Color.red);
-        g.fillRect(x, y, anchura, altura);
-
-        g.setColor(Color.black);
-        g.drawRect(x, y, anchura, altura);
+        int x = (this.anchura < 0) ? this.x + anchura : this.x;
+        int y = (this.altura < 0) ? this.y + altura : this.y;
+        
+        g.drawRect( x, y, Math.abs( anchura ), Math.abs( altura ) );
     }
 }
