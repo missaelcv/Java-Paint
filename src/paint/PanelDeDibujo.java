2baf442a0b632b482185eb82a24768919a35ab4c
missaelcv <missaelcv0220@gmail.com>
2021-10-28 23:11:52 -0400

    Cambios de dibujoLibre

--- src/paint/PanelDeDibujo.java
+++ src/paint/PanelDeDibujo.java
@@ -11,96 +11,98 @@ import java.util.ArrayList;
 import javax.swing.JPanel;
 import figuras.*;
 import figuras.DibujoLibre;
+import java.util.Stack;
 import javax.swing.SwingUtilities;
 
-public class PanelDeDibujo extends JPanel {
+public class PanelDeDibujo extends javax.swing.JPanel {
 
-    int almacenaNumerito;
+    Color colorDePrimerPlano;
+
+    public JPanel getPanelDeHerramientas() {
+        JPanel panelDeHerramientas = null;
+        return panelDeHerramientas;
+    }
+    Color colorDeSegundoPlano;
+  
     Figura figuraActual;
     ArrayList<Figura> figuras;
-
-    DibujoLibre punto;
-
-    public int getAlmacenaNumerito() {
-        return almacenaNumerito;
+    Stack<Figura> figurasDeshechas;
+    
+    
+    public void deshacer() {
+        if( !figuras.isEmpty() ) {
+            figurasDeshechas.push( figuras.remove( figuras.size() - 1 ) );
+            repaint();
+        }
     }
 
-    public void setAlmacenaNumerito(int almacenaNumerito) {
-        this.almacenaNumerito = almacenaNumerito;
+    public void rehacer() {
+        if( !figurasDeshechas.isEmpty() ) {
+            figuras.add( figurasDeshechas.pop() );
+            repaint();
+        }
     }
-
+    
     public PanelDeDibujo() {
-
+        initComponents();
+        
+        colorDePrimerPlano = Color.BLACK;
+        colorDeSegundoPlano = Color.WHITE;
+        
         figuras = new ArrayList<>();
-
-        setBackground(Color.WHITE);
-
-        class Manejador extends MouseAdapter {
+        figurasDeshechas = new Stack<>();
+        
+        setBackground( Color.WHITE );
+        
+        
+        class Manejador extends MouseAdapter{
 
             @Override
             public void mousePressed(MouseEvent evento) {
-
-                if (evento.getButton() == MouseEvent.BUTTON1) {
-
-                    switch (getAlmacenaNumerito()) {
-                        case 1:
-                            figuraActual = new Rectangulo(evento.getPoint());
-                            break;
-                        case 2:
-                            figuraActual = new Linea(evento.getPoint());
-                            break;
-                        case 3:
-                            figuraActual = new Triangulo(evento.getPoint());
-                            break;
-                        case 4:
-                            figuraActual = new Ovalo(evento.getPoint());
-                            break;
-                         case 5:
-                            figuraActual = new Rombo(evento.getPoint());
-                            break;
-                        default:
-                            figuraActual = new DibujoLibre(evento.getPoint());
-                            break;
-                    }
-
-                    figuras.add(figuraActual);
-                    repaint();
+                if( btnLinea.isSelected() ) {
+                    figuraActual = new Linea( colorDePrimerPlano, evento.getPoint() );
                 }
-
-                if (evento.getButton() == MouseEvent.BUTTON3) {
-
-                    figuraActual = new DibujoLibre(evento.getPoint());
-                    figuras.add(figuraActual);
+                else if( btnRectangulo.isSelected() ) {
+                    figuraActual = new Rectangulo( colorDePrimerPlano, evento.getPoint() );
                 }
+                else if( btnOvalo.isSelected() ) {
+                    figuraActual = new Ovalo( colorDePrimerPlano, evento.getPoint() );
+                }
+                else {
+                    figuraActual = new DibujoLibre( colorDePrimerPlano, evento.getPoint() );
+                    
+                }
+                
+                try {
+                    Image imagen = new ImageIcon(getClass().getResource("/imagenes/lapiz.png")).getImage();
+                    setCursor(Toolkit.getDefaultToolkit().createCustomCursor(imagen, new Point(0, 63), "custom cursor"));
+                } catch (Exception excepcion) {
+                    System.out.println("Ocurrio un error y no se pudo cargar la imagen");
+                    excepcion.printStackTrace();
+                }
+                
+                figuras.add(figuraActual );
+                repaint();
             }
 
             @Override
-            public void mouseDragged(MouseEvent evento) {
-
-                if (SwingUtilities.isLeftMouseButton(evento)) {
-
-                    figuraActual.actualizar(evento.getPoint());
-                }
-
-                if (SwingUtilities.isRightMouseButton(evento)) {
-
-                    figuraActual.actualizar(evento.getPoint());
-                }
+            public void mouseDragged(MouseEvent evento ) {
+                figuraActual.actualizar(evento.getPoint());
                 repaint();
             }
         }
-
+        
         Manejador manejador = new Manejador();
-        this.addMouseListener(manejador);
-        this.addMouseMotionListener(manejador);
+        this.addMouseListener( manejador );
+        this.addMouseMotionListener( manejador );
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
+        
         g.setColor(Color.blue);
-
+        
         int distancia = 20;
         int total = 0;
         for (int y = distancia * 2; y < getHeight() && total < 27; y += distancia) {
@@ -108,12 +110,11 @@ public class PanelDeDibujo extends JPanel {
             g.drawString("" + total, 0, y);
             g.drawLine(10, y, getWidth() - 10, y);
         }
-
-        g.setColor(Color.red);
+                
+        g.setColor(Color.RED);
         g.drawLine(distancia * 2, 0, distancia * 2, getHeight());
-
-        for (Figura figuraActual : figuras) {
-            figuraActual.dibujar(g);
-        }
+        
+        for (Figura figura : figuras) {
+            figura.dibujar(g);
+        }   
     }
-}
