317df1bd1cbd1cf4820d2e485de6755e43a6b560
missaelcv <missaelcv0220@gmail.com>
2021-11-08 21:19:48 -0400

    Nuevos Punto de Ovalo

--- src/figuras/Ovalo.java
+++ src/figuras/Ovalo.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package figuras;
 
@@ -11,7 +10,7 @@ import java.awt.Point;
 
 /**
  *
- * @author Misael Caceres
+ * @author josearielpereyra
  */
 public class Ovalo extends Rectangulo{
    
