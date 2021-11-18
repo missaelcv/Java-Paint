6676cbf616e737e491dbd86428294645ab8549d7
missaelcv <missaelcv0220@gmail.com>
2021-11-17 20:51:09 -0400

    Color Rojo

--- src/figuras/Rombo.java
+++ src/figuras/Rombo.java
@@ -28,13 +28,14 @@ public class Rombo extends Rectangulo {
         int y = this.altura < 0 ? this.y + this.altura : this.y;
         int anchura = Math.abs(this.anchura);
         int altura = Math.abs(this.altura);
-
+        
         Point punto1 = new Point(x, y + altura / 2); 
         Point punto2 = new Point(x + anchura/2, y);
         Point punto3 = new Point(x + anchura, y + altura / 2);
         Point punto4 = new Point(x + anchura / 2, y + altura);
         
-        g.setColor(Color.BLUE);
+        g.setColor(Color.RED);
+        g.fillPolygon( new int []{ punto1.x, punto2.x, punto3.x, punto4.x}, new int []{ punto1.y, punto2.y, punto3.y, punto4.y}, 4 );
         g.drawPolygon( new int []{ punto1.x, punto2.x, punto3.x, punto4.x}, new int []{ punto1.y, punto2.y, punto3.y, punto4.y}, 4 );
     }
 }
