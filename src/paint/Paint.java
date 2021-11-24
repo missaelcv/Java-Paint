356397e3a7a4486334eac9d233af1511f732c1ad
missaelcv <missaelcv0220@gmail.com>
2021-11-23 23:42:11 -0400

    main con borrador

--- src/paint/Paint.java
+++ src/paint/Paint.java
@@ -15,15 +15,7 @@ public class Paint {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        VentanaPrincipal P = new VentanaPrincipal();
-        P.setVisible(true);
-        
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+     public static void main(String args[]) {
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -40,15 +32,10 @@ public class Paint {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new VentanaPrincipal().setVisible(true);
             }
         });
-        
-        
     }
 }
