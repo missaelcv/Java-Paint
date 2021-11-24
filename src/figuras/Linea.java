a5b1b7616e1988741734d0012a57f779e0b5e870
missaelcv <missaelcv0220@gmail.com>
2021-11-23 23:52:28 -0400

    linea Implementada

--- src/figuras/Linea.java
+++ src/figuras/Linea.java
@@ -16,24 +16,24 @@ public class Linea extends Figura{
     Point puntoInicial;
     Point puntoFinal;
 
-    public Linea(Color color, Point puntoInicial, Point puntoFinal) {
-        super(color);
+    public Linea(Color color,Color color2, Boolean R, Point puntoInicial, Point puntoFinal) {
+        super(color,color2, R);
         this.puntoInicial = puntoInicial;
         this.puntoFinal = puntoFinal;
     }
 
-    public Linea(Color color, Point puntoInicial) {
-        this(color, puntoInicial, puntoInicial);
+    public Linea(Color color,Color color2, Boolean R, Point puntoInicial) {
+        this(color,color2, R, puntoInicial, puntoInicial);
     }
 
-    public void actualizar( Point puntoFinal ) {
+    public void actualizar(Point puntoFinal) {
         this.puntoFinal = puntoFinal;
     }
 
     public void dibujar(Graphics g) {
-        if(puntoInicial != null && puntoFinal != null) {
-            g.setColor( getColorDePrimerPlano() );
-            g.drawLine( puntoInicial.x, puntoInicial.y, puntoFinal.x, puntoFinal.y );
+        if (puntoInicial != null && puntoFinal != null) {
+            g.setColor(getColorDePrimerPlano());
+            g.drawLine(puntoInicial.x, puntoInicial.y, puntoFinal.x, puntoFinal.y);
         }
     }
 }
