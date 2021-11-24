bac101499602ddeab40e575fd65e58135e5027da
missaelcv <missaelcv0220@gmail.com>
2021-11-23 23:52:00 -0400

    figura implementada

--- src/figuras/Figura.java
+++ src/figuras/Figura.java
@@ -13,14 +13,35 @@ import java.awt.Point;
  * @author josearielpereyra
  */
 public abstract class Figura {
-    private Color colorDePrimerPlano;
-    
-    Figura(Color colorDePrimerPlano) {
+     private Color colorDePrimerPlano;
+    private Color colorDeSegundoPlano;
+    private Boolean R;
+
+    Figura(Color colorDePrimerPlano, Color colorDeSegundoPlano, Boolean R) {
         this.colorDePrimerPlano = colorDePrimerPlano;
+        this.R = R;
+        this.colorDeSegundoPlano = colorDeSegundoPlano;
     }
-    
-    public abstract void actualizar( Point puntoFinal );
-    public abstract void dibujar( Graphics g );
+
+    public Color getColorDeSegundoPlano() {
+        return colorDeSegundoPlano;
+    }
+
+    public void setColorDeSegundoPlano(Color colorDeSegundoPlano) {
+        this.colorDeSegundoPlano = colorDeSegundoPlano;
+    }
+
+    public Boolean getR() {
+        return R;
+    }
+
+    public void setR(Boolean R) {
+        this.R = R;
+    }
+
+    public abstract void actualizar(Point puntoFinal);
+
+    public abstract void dibujar(Graphics g);
 
     public Color getColorDePrimerPlano() {
         return colorDePrimerPlano;
@@ -29,4 +50,5 @@ public abstract class Figura {
     public void setColorDePrimerPlano(Color colorDePrimerPlano) {
         this.colorDePrimerPlano = colorDePrimerPlano;
     }
+
 }
