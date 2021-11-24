3f7814fd5bb228ab5cb08c3f0a8e75090bbc58e5
missaelcv <missaelcv0220@gmail.com>
2021-11-23 23:53:07 -0400

    rectangulo implementada

--- src/figuras/Rectangulo.java
+++ src/figuras/Rectangulo.java
@@ -18,25 +18,30 @@ public class Rectangulo extends Figura{
     int anchura;
     int altura;
 
-    public Rectangulo(Color color, Point puntoInicial) {
-        super(color);
+    public Rectangulo(Color color, Color color2, Boolean R, Point puntoInicial) {
+        super(color, color2, R);
         this.x = puntoInicial.x;
         this.y = puntoInicial.y;
         this.anchura = 1;
         this.altura = 1;
     }
 
-    public void actualizar( Point puntoActual ) {
+    public void actualizar(Point puntoActual) {
         this.anchura = puntoActual.x - x;
         this.altura = puntoActual.y - y;
     }
 
     public void dibujar(Graphics g) {
-        g.setColor( getColorDePrimerPlano() );
-        
+
         int x = (this.anchura < 0) ? this.x + anchura : this.x;
         int y = (this.altura < 0) ? this.y + altura : this.y;
-        
-        g.drawRect( x, y, Math.abs( anchura ), Math.abs( altura ) );
+
+        if (getR()) {
+            g.setColor(getColorDeSegundoPlano());
+            g.fillRect(x, y, Math.abs(anchura), Math.abs(altura));
+        }
+        g.setColor(getColorDePrimerPlano());
+        g.drawRect(x, y, Math.abs(anchura), Math.abs(altura));
+
     }
 }
