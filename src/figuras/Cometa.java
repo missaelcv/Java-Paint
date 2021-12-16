2f31539882bbbd8039fb1b82aeb4597dd5cab3e1
missaelcv <missaelcv0220@gmail.com>
2021-12-15 23:45:45 -0400

    la cometa crece de manera apropiada

--- src/figuras/Cometa.java
+++ src/figuras/Cometa.java
@@ -2,15 +2,15 @@ package figuras;
 
 import java.awt.*;
 
-public class Cometa extends FiguraBordeable {
+public class Cometa extends FiguraRellenable {
 
     int x;
     int y;
     int anchura;
     int altura;
 
-    public Cometa(Color color, Color color2, Boolean relleno, Point puntoInicial, Boolean agregarRectangulo) {
-        super(color, color2, relleno, agregarRectangulo);
+    public Cometa(Color color, Color color2, Boolean relleno, Point puntoInicial) {
+        super(color, color2, relleno);
         this.x = puntoInicial.x;
         this.y = puntoInicial.y;
         this.anchura = 1;
@@ -41,9 +41,8 @@ public class Cometa extends FiguraBordeable {
         g.setColor(getColorDePrimerPlano());
         g.drawPolygon(new int[]{punto1.x, punto2.x, punto3.x, punto4.x}, new int[]{punto1.y, punto2.y, punto3.y, punto4.y}, 4);
 
-        if (getAgregarRectangulo()) {
-            g.drawRect(x, y, Math.abs(anchura), Math.abs(altura));
-        }
+        this.setRectangulo(new Rectangle(x, y, anchura, altura));
+        super.dibujar(g);
 
     }
 }
