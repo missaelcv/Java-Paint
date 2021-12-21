41b7a8ec560fc78fdd0a588f1e077cae5f733a7d
missaelcv <missaelcv0220@gmail.com>
2021-12-20 21:40:10 -0400

    semicirculo con puntos

--- src/figuras/SemiCirculo.java
+++ src/figuras/SemiCirculo.java
@@ -2,7 +2,7 @@ package figuras;
 
 import java.awt.*;
 
-public class SemiCirculo extends FiguraBordeable {
+public class SemiCirculo extends FiguraRellenable {
 
     Point puntoInicial;
     Point puntoFinal;
@@ -11,8 +11,8 @@ public class SemiCirculo extends FiguraBordeable {
     int anchura;
     int altura;
 
-    public SemiCirculo(Color color, Color color2, Boolean relleno, Point puntoInicial, Boolean agregarRectangulo) {
-        super(color, color2, relleno, agregarRectangulo);
+    public SemiCirculo(Color color, Color color2, Boolean relleno, Point puntoInicial) {
+        super(color, color2, relleno);
         this.puntoInicial = puntoInicial;
         this.puntoFinal = puntoFinal;
         this.x = puntoInicial.x;
@@ -28,8 +28,13 @@ public class SemiCirculo extends FiguraBordeable {
     }
 
     public void dibujar(Graphics g) {
-        if (puntoInicial != null && puntoFinal != null) {
 
+        int x = this.anchura < 0 ? this.x + this.anchura : this.x;
+        int y = this.altura < 0 ? this.y + this.altura : this.y;
+        int anchura = Math.abs(this.anchura);
+        int altura = Math.abs(this.altura);
+        
+        if (puntoInicial != null && puntoFinal != null) {
             if (getRelleno()) {
                 g.setColor(getColorDeSegundoPlano());
                 g.fillArc(x, y, anchura, puntoFinal.y, 0, 180);
@@ -38,9 +43,8 @@ public class SemiCirculo extends FiguraBordeable {
             g.drawLine(puntoInicial.x, ((int) (puntoFinal.y * 0.50) + y), puntoFinal.x, ((int) (puntoFinal.y * 0.50) + y));
             g.drawArc(x, Math.abs(y), anchura, Math.abs(puntoFinal.y), 0, 180);
 
-            if (getAgregarRectangulo()) {
-                g.drawRect(x, y, Math.abs(anchura), Math.abs(altura));
-            }
+            this.setRectangulo(new Rectangle(x, y, anchura, altura));
+            super.dibujar(g);
         }
     }
 }
